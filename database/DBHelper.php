ede9c3560088c1b932a49139774cc0ca0a0aee6a
Jeric James Viernes <jericviernes06@gmail.com>
2023-09-05 16:13:01 +0800

    add return $dbh;

--- database/DBHelper.php
+++ database/DBHelper.php
@@ -6,6 +6,7 @@ class DatabaseHandler{
             $username = "root";
             $password = "";
             $dbh = new PDO('mysql:host=localhost; dbname = otcmedicineis', $username, $password);
+            return $dbh;
         } catch (PDOException $e) {
             echo "Error!: " . $e->getMessage() . "<br/>";
             die();
